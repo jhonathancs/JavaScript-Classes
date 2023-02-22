85386cfd2cf0cc62cfb432f3861d3bc1ad5e48a4
Jhonathan Costa <jhonathan_jcs@hotmail.com>
2023-02-22 15:54:59 -0400

    finals

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const characterId = document.getElementById("characterId");
+const characterId = document.getElementById("characterID");
 const btnGO = document.getElementById("btn-go");
 const content = document.getElementById("content");
 const img = document.getElementById("img");
 
 const fetchApi = (value) => {
   const result = fetch(`
-  https://rickandmortyapi.com/api/character/${value}`
-  )
-  .then((res) => res.json())
-  .then((data) =>{
-    console.log(data)
-  });
-//   console.log(result);
+  https://rickandmortyapi.com/api/character/${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //  const { status: vivo, name: nome } = data
+       console.log(data);
+      // return data
+      //  console.log(`meu mome é ${nome} e eu ainda estou ${vivo}`)
+      return data;
+    });
+  return result;
 };
 
-fetchApi(10);
 
-btnGO.addEventListener("click", () => {
-
+btnGO.addEventListener("click", async (event) => {
+  event.preventDefault();
+  const result = await fetchApi(characterId.value);
+  content.textContent = `${JSON.stringify(result,undefined,2)}`
+  img.src = `${result.image}`
 });
